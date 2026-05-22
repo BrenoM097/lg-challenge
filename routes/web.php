fdd39e333c1a6891170253e40497d7562d94e134
Breno Melo Porcel <melobreno89@gmail.com>
2026-05-22 16:30:09 -0300

    feat: Add redirect from root to dashboard

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductivityController;
 use App\Http\Controllers\AiSimulationController;
 
-
+Route::redirect('/', '/dashboard');
 Route::get('/dashboard', [ProductivityController::class, 'index']);
 Route::get('/dashboard/check-api-key', [AiSimulationController::class, 'checkApiKey']);
 Route::post('/dashboard/simulate', [AiSimulationController::class, 'simulate']);
